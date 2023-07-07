b9510cd6bab8410e1612c11e55b0344b0d4ce08b
Braden Mars <bradenmars@bradenmars.me>
2023-07-07 02:37:48 -0500

    feat(projenrc): add and init direnv
    Signed-off-by: Braden Mars <bradenmars@bradenmars.me>

--- .projenrc.ts
+++ .projenrc.ts
@@ -1,6 +1,7 @@
 import { javascript } from "projen";
 import {MonorepoProject} from '@arroyodev-llc/projen.project.nx-monorepo'
 import {ToolVersions} from '@arroyodev-llc/projen.component.tool-versions'
+import {DirEnv} from '@arroyodev-llc/projen.component.dir-env'
 
 const monorepo = new MonorepoProject({
   name: "crisiscleanup-infrastructure",
@@ -16,11 +17,17 @@ const monorepo = new MonorepoProject({
   pnpmVersion: '8.6.6'
 });
 
-new ToolVersions(monorepo, {
+const tools = new ToolVersions(monorepo, {
   tools: {
+    direnv: ['2.32.3'],
     nodejs: [monorepo.package.minNodeVersion!],
-    pnpm: [monorepo.package.pnpmVersion!]
+    pnpm: [monorepo.package.pnpmVersion!],
   }
+})
+
+new DirEnv(monorepo).buildDefaultEnvRc({
+  localEnvRc: '.envrc.local',
+  minDirEnvVersion: tools.versionsOf('direnv')[0]!,
 })
 
 monorepo.synth();
