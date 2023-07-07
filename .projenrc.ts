2076d9cdef817f2afed232b93f07eb05a2555499
Braden Mars <bradenmars@bradenmars.me>
2023-07-07 14:09:14 -0500

    feat: add cdk8s-cli, set author
    Signed-off-by: Braden Mars <bradenmars@bradenmars.me>

--- .projenrc.ts
+++ .projenrc.ts
@@ -7,14 +7,24 @@ const monorepo = new MonorepoProject({
   name: "crisiscleanup-infrastructure",
   devDeps: [
     "@arroyodev-llc/projen.project.nx-monorepo",
+    "@arroyodev-llc/projen.project.typescript",
     "@arroyodev-llc/projen.component.tool-versions",
     "@arroyodev-llc/projen.component.dir-env",
-    "@aws-prototyping-sdk/nx-monorepo"
+    "@aws-prototyping-sdk/nx-monorepo",
+    "cdk8s-cli",
   ],
   packageManager: javascript.NodePackageManager.PNPM,
   projenrcTs: true,
   minNodeVersion: '18.16.0',
-  pnpmVersion: '8.6.6'
+  pnpmVersion: '8.6.6',
+  authorName: 'CrisisCleanup',
+  authorEmail: 'help@crisiscleanup.org',
+  authorOrganization: true,
+  authorUrl: 'https://crisiscleanup.org',
+  namingScheme: {
+    scope: '@crisiscleanup',
+    packagesDir: 'packages'
+  }
 });
 
 const tools = new ToolVersions(monorepo, {
