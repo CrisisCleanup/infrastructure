1d6cf5f430b40b8db0b916b431393b90b68991b5
Braden Mars <bradenmars@bradenmars.me>
2023-07-08 05:08:20 -0500

    feat(projenrc): setup git-hooks + lint config

--- .projenrc.ts
+++ .projenrc.ts
@@ -1,7 +1,10 @@
-import { javascript } from "projen";
-import {MonorepoProject} from '@arroyodev-llc/projen.project.nx-monorepo'
-import {ToolVersions} from '@arroyodev-llc/projen.component.tool-versions'
-import {DirEnv} from '@arroyodev-llc/projen.component.dir-env'
+import { javascript, typescript } from "projen";
+import { MonorepoProject } from "@arroyodev-llc/projen.project.nx-monorepo";
+import { ToolVersions } from "@arroyodev-llc/projen.component.tool-versions";
+import { DirEnv } from "@arroyodev-llc/projen.component.dir-env";
+import { LintConfig } from "@arroyodev-llc/projen.component.linting";
+import { ExtensionMatch, GitHooks, LintStaged, ValidGitHooks } from "@arroyodev-llc/projen.component.git-hooks";
+import { NodePackageUtils } from "@aws-prototyping-sdk/nx-monorepo";
 
 const monorepo = new MonorepoProject({
   name: "crisiscleanup-infrastructure",
@@ -11,6 +14,7 @@ const monorepo = new MonorepoProject({
     "@arroyodev-llc/projen.component.tool-versions",
     "@arroyodev-llc/projen.component.dir-env",
     "@arroyodev-llc/projen.component.linting",
+    "@arroyodev-llc/projen.component.git-hooks",
     "@aws-prototyping-sdk/nx-monorepo",
     "cdk8s-cli",
   ],
@@ -25,6 +29,11 @@ const monorepo = new MonorepoProject({
   namingScheme: {
     scope: '@crisiscleanup',
     packagesDir: 'packages'
+  },
+  tsconfig: {
+    compilerOptions: {
+      isolatedModules: false
+    }
   }
 });
 
@@ -36,9 +45,42 @@ const tools = new ToolVersions(monorepo, {
   }
 })
 
+new LintConfig(monorepo)
+
 new DirEnv(monorepo).buildDefaultEnvRc({
   localEnvRc: '.envrc.local',
   minDirEnvVersion: tools.versionsOf('direnv')[0]!,
+})
+
+// Setup githooks
+monorepo.applyRecursive((project) => {
+  if (project instanceof typescript.TypeScriptProject) {
+    new LintStaged(project, {
+      entries: [
+        {
+          extensions: [ExtensionMatch.TS, ExtensionMatch.JS],
+          commands: [NodePackageUtils.command.exec(project.package.packageManager, 'eslint --cache --fix --no-error-on-unmatched-pattern')]
+        },
+        {
+          extensions: [ExtensionMatch.YAML],
+          commands: [NodePackageUtils.command.exec(project.package.packageManager, 'prettier --write')]
+        }
+      ]
+    })
+  }
+  // ensure all readonly files are ignored
+  project.files.filter((file) => file.readonly).forEach((file) => {
+    const lintConfig = LintConfig.of(project)
+    lintConfig?.prettier?.addIgnorePattern?.(file.path)
+    lintConfig?.eslint?.addIgnorePattern?.(file.path)
+  })
+}, {immediate: false, includeSelf: true})
+
+new GitHooks(monorepo, {
+  hooks: {
+    [ValidGitHooks.PreCommit]: NodePackageUtils.command.exec(monorepo.package.packageManager, 'lint-staged')
+  },
+  preserveUnused: true
 })
 
 monorepo.synth();
