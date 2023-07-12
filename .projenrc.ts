8f6131202a7bb7ba9406e670f89f1bf1c68a9388
Braden Mars <bradenmars@bradenmars.me>
2023-07-12 00:41:22 -0500

    style: format all
    Signed-off-by: Braden Mars <bradenmars@bradenmars.me>

--- .projenrc.ts
+++ .projenrc.ts
@@ -1,86 +1,109 @@
-import { javascript, typescript } from "projen";
-import { MonorepoProject } from "@arroyodev-llc/projen.project.nx-monorepo";
-import { ToolVersions } from "@arroyodev-llc/projen.component.tool-versions";
-import { DirEnv } from "@arroyodev-llc/projen.component.dir-env";
-import { LintConfig } from "@arroyodev-llc/projen.component.linting";
-import { ExtensionMatch, GitHooks, LintStaged, ValidGitHooks } from "@arroyodev-llc/projen.component.git-hooks";
-import { NodePackageUtils } from "@aws-prototyping-sdk/nx-monorepo";
+import { DirEnv } from '@arroyodev-llc/projen.component.dir-env'
+import {
+	ExtensionMatch,
+	GitHooks,
+	LintStaged,
+	ValidGitHooks,
+} from '@arroyodev-llc/projen.component.git-hooks'
+import { LintConfig } from '@arroyodev-llc/projen.component.linting'
+import { ToolVersions } from '@arroyodev-llc/projen.component.tool-versions'
+import { MonorepoProject } from '@arroyodev-llc/projen.project.nx-monorepo'
+import { NodePackageUtils } from '@aws-prototyping-sdk/nx-monorepo'
+import { javascript, typescript } from 'projen'
 
 const monorepo = new MonorepoProject({
-  name: "crisiscleanup-infrastructure",
-  devDeps: [
-    "@arroyodev-llc/projen.project.nx-monorepo",
-    "@arroyodev-llc/projen.project.typescript",
-    "@arroyodev-llc/projen.component.tool-versions",
-    "@arroyodev-llc/projen.component.dir-env",
-    "@arroyodev-llc/projen.component.linting",
-    "@arroyodev-llc/projen.component.git-hooks",
-    "@aws-prototyping-sdk/nx-monorepo",
-    "cdk8s-cli",
-  ],
-  packageManager: javascript.NodePackageManager.PNPM,
-  projenrcTs: true,
-  minNodeVersion: '18.16.0',
-  pnpmVersion: '8.6.6',
-  authorName: 'CrisisCleanup',
-  authorEmail: 'help@crisiscleanup.org',
-  authorOrganization: true,
-  authorUrl: 'https://crisiscleanup.org',
-  namingScheme: {
-    scope: '@crisiscleanup',
-    packagesDir: 'packages'
-  },
-  tsconfig: {
-    compilerOptions: {
-      isolatedModules: false
-    }
-  }
-});
+	name: 'crisiscleanup-infrastructure',
+	devDeps: [
+		'@arroyodev-llc/projen.project.nx-monorepo',
+		'@arroyodev-llc/projen.project.typescript',
+		'@arroyodev-llc/projen.component.tool-versions',
+		'@arroyodev-llc/projen.component.dir-env',
+		'@arroyodev-llc/projen.component.linting',
+		'@arroyodev-llc/projen.component.git-hooks',
+		'@aws-prototyping-sdk/nx-monorepo',
+		'cdk8s-cli',
+	],
+	packageManager: javascript.NodePackageManager.PNPM,
+	projenrcTs: true,
+	minNodeVersion: '18.16.0',
+	pnpmVersion: '8.6.6',
+	authorName: 'CrisisCleanup',
+	authorEmail: 'help@crisiscleanup.org',
+	authorOrganization: true,
+	authorUrl: 'https://crisiscleanup.org',
+	namingScheme: {
+		scope: '@crisiscleanup',
+		packagesDir: 'packages',
+	},
+	tsconfig: {
+		compilerOptions: {
+			isolatedModules: false,
+		},
+	},
+})
 
 const tools = new ToolVersions(monorepo, {
-  tools: {
-    direnv: ['2.32.3'],
-    nodejs: [monorepo.package.minNodeVersion!],
-    pnpm: [monorepo.package.pnpmVersion!],
-  }
+	tools: {
+		direnv: ['2.32.3'],
+		nodejs: [monorepo.package.minNodeVersion!],
+		pnpm: [monorepo.package.pnpmVersion!],
+	},
 })
 
 new LintConfig(monorepo)
 
 new DirEnv(monorepo).buildDefaultEnvRc({
-  localEnvRc: '.envrc.local',
-  minDirEnvVersion: tools.versionsOf('direnv')[0]!,
+	localEnvRc: '.envrc.local',
+	minDirEnvVersion: tools.versionsOf('direnv')[0]!,
 })
 
 // Setup githooks
-monorepo.applyRecursive((project) => {
-  if (project instanceof typescript.TypeScriptProject) {
-    new LintStaged(project, {
-      entries: [
-        {
-          extensions: [ExtensionMatch.TS, ExtensionMatch.JS],
-          commands: [NodePackageUtils.command.exec(project.package.packageManager, 'eslint --cache --fix --no-error-on-unmatched-pattern')]
-        },
-        {
-          extensions: [ExtensionMatch.YAML],
-          commands: [NodePackageUtils.command.exec(project.package.packageManager, 'prettier --write')]
-        }
-      ]
-    })
-  }
-  // ensure all readonly files are ignored
-  project.files.filter((file) => file.readonly).forEach((file) => {
-    const lintConfig = LintConfig.of(project)
-    lintConfig?.prettier?.addIgnorePattern?.(file.path)
-    lintConfig?.eslint?.addIgnorePattern?.(file.path)
-  })
-}, {immediate: false, includeSelf: true})
+monorepo.applyRecursive(
+	(project) => {
+		if (project instanceof typescript.TypeScriptProject) {
+			new LintStaged(project, {
+				entries: [
+					{
+						extensions: [ExtensionMatch.TS, ExtensionMatch.JS],
+						commands: [
+							NodePackageUtils.command.exec(
+								project.package.packageManager,
+								'eslint --cache --fix --no-error-on-unmatched-pattern',
+							),
+						],
+					},
+					{
+						extensions: [ExtensionMatch.YAML],
+						commands: [
+							NodePackageUtils.command.exec(
+								project.package.packageManager,
+								'prettier --write',
+							),
+						],
+					},
+				],
+			})
+		}
+		// ensure all readonly files are ignored
+		project.files
+			.filter((file) => file.readonly)
+			.forEach((file) => {
+				const lintConfig = LintConfig.of(project)
+				lintConfig?.prettier?.addIgnorePattern?.(file.path)
+				lintConfig?.eslint?.addIgnorePattern?.(file.path)
+			})
+	},
+	{ immediate: false, includeSelf: true },
+)
 
 new GitHooks(monorepo, {
-  hooks: {
-    [ValidGitHooks.PreCommit]: NodePackageUtils.command.exec(monorepo.package.packageManager, 'lint-staged')
-  },
-  preserveUnused: true
+	hooks: {
+		[ValidGitHooks.PreCommit]: NodePackageUtils.command.exec(
+			monorepo.package.packageManager,
+			'lint-staged',
+		),
+	},
+	preserveUnused: true,
 })
 
-monorepo.synth();
+monorepo.synth()
