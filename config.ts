c2fae32bb0932d754cd5d4bbef58c9b7662da0fa
Braden Mars <bradenmars@bradenmars.me>
2023-07-20 20:21:18 -0500

    feat: add root config file with defaults
    Signed-off-by: Braden Mars <bradenmars@bradenmars.me>

---
+++ config.ts
@@ -0,0 +1,11 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { defineConfig, baseApiConfig } from '@crisiscleanup/config'
+
+export default defineConfig({
+	$meta: { name: 'crisiscleanup' },
+	api: baseApiConfig,
+	cdkEnvironment: {
+		region: process.env.CDK_DEFAULT_REGION ?? 'us-east-1',
+		account: process.env.CDK_DEFAULT_ACCOUNT!,
+	},
+})
