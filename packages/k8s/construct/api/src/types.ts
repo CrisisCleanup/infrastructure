c7dca2e140e493e2e8fb0d2fd80ebbd5cd2200e9
Braden Mars <bradenmars@bradenmars.me>
2023-07-21 04:00:42 -0500

    refactor(k8s.construct.api): extract types module
    Signed-off-by: Braden Mars <bradenmars@bradenmars.me>

---
+++ packages/k8s/construct/api/src/types.ts
@@ -0,0 +1,43 @@
+import {
+	type ApiAppConfig,
+	type ApiAppSecrets,
+	type FlattenObject,
+	type ScreamingSnakeCaseProperties,
+} from '@crisiscleanup/config'
+import { type DeploymentProps } from '@crisiscleanup/k8s.construct.component'
+import type * as kplus from 'cdk8s-plus-24'
+
+export interface ApiConfigProps {
+	config: ScreamingSnakeCaseProperties<FlattenObject<ApiAppConfig, '_'>>
+	secrets: ScreamingSnakeCaseProperties<FlattenObject<ApiAppSecrets, '_'>>
+}
+
+export interface ApiProps extends DeploymentProps {
+	config?: IApiConfig
+}
+
+export interface CeleryProps extends ApiProps {
+	queues: string[]
+	name?: string
+	concurrency?: number
+	args?: string[]
+}
+
+export interface ApiWSGIProps extends ApiProps {
+	workers?: number
+	threads?: number
+}
+
+export interface ApiASGIProps extends ApiProps {
+	workers?: number
+}
+
+export interface IHttpProbable {
+	httpProbePath: string
+}
+
+export interface IApiConfig {
+	configMap: kplus.ConfigMap
+	configSecret: kplus.Secret
+	readonly envFrom: kplus.EnvFrom[]
+}
