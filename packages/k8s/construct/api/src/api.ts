4eee7b26a32abd2d3f4752b5a93b2500f13b1b5c
Braden Mars <bradenmars@bradenmars.me>
2023-07-21 03:56:34 -0500

    feat(k8s.construct.api): remove `Backend/Celery` constructs, `ApiComponent` abstract, config improvements
    Signed-off-by: Braden Mars <bradenmars@bradenmars.me>

--- packages/k8s/construct/api/src/api.ts
+++ packages/k8s/construct/api/src/api.ts
@@ -9,47 +9,57 @@ import {
 	Component,
 	type DeploymentProps,
 } from '@crisiscleanup/k8s.construct.component'
-import { Duration } from 'cdk8s'
+import { Chart, Duration } from 'cdk8s'
 import * as kplus from 'cdk8s-plus-24'
 import { Construct } from 'constructs'
-// import yaml from 'js-yaml'
 
-export interface BackendConfigProps {
+export interface ApiConfigProps {
 	config: ScreamingSnakeCaseProperties<FlattenObject<ApiAppConfig, '_'>>
 	secrets: ScreamingSnakeCaseProperties<FlattenObject<ApiAppSecrets, '_'>>
 }
 
-export interface BackendApiProps extends DeploymentProps {
-	config: BackendConfig
+export interface ApiProps extends DeploymentProps {
+	config?: IApiConfig
 }
 
-export interface CeleryQueueProps {
-	name: string
+export interface CeleryProps extends ApiProps {
+	queues: string[]
+	name?: string
+	concurrency?: number
 	args?: string[]
 }
 
-export interface CeleryProps extends BackendApiProps {
-	queues?: CeleryQueueProps[]
+export interface ApiWSGIProps extends ApiProps {
+	workers?: number
+	threads?: number
 }
 
-export interface BackendProps {
-	asgi: Omit<BackendApiProps, 'config'>
-	wsgi: Omit<BackendApiProps, 'config'>
-	celery: Omit<CeleryProps, 'config'>
-	config: BackendConfigProps['config']
-	secrets: BackendConfigProps['secrets']
-	stage?: string
+export interface ApiASGIProps extends ApiProps {
+	workers?: number
 }
 
-export class BackendConfig extends Construct {
+export interface IHttpProbable {
+	httpProbePath: string
+}
+
+interface IApiConfig {
 	configMap: kplus.ConfigMap
 	configSecret: kplus.Secret
+	readonly envFrom: kplus.EnvFrom[]
+}
 
-	constructor(scope: Construct, id: string, props: BackendConfigProps) {
+export class ApiConfig extends Construct implements IApiConfig {
+	static of(construct: Construct): ApiConfig | undefined {
+		return Chart.of(construct)
+			.node.findAll()
+			.find((c) => c instanceof ApiConfig) as ApiConfig
+	}
+
+	readonly configMap: kplus.ConfigMap
+	readonly configSecret: kplus.Secret
+
+	constructor(scope: Construct, id: string, props: ApiConfigProps) {
 		super(scope, id)
-
-		// const stage = this.node.getContext('') as string
-
 		this.configMap = new kplus.ConfigMap(this, 'config', {
 			data: stringifyObjectValues(props.config),
 		})
@@ -57,25 +67,6 @@ export class BackendConfig extends Construct {
 		this.configSecret = new kplus.Secret(this, 'config-secret', {
 			stringData: stringifyObjectValues(props.secrets),
 		})
-
-		// const secretObjects = Object.entries(
-		// 	stringifyObjectValues(props.secrets),
-		// ).map(([key]) => ({
-		// 	objectAlias: key,
-		// 	objectName: `${stage}/${key}`,
-		// 	objectType: 'secretsmanager',
-		// }))
-
-		// new SecretProviderClass(this, 'aws-secrets', {
-		// 	metadata: { name: 'aws-secrets' },
-		// 	spec: {
-		// 		provider: 'aws',
-		// 		parameters: {
-		// 			region: 'us-east-1',
-		// 			objects: yaml.dump(secretObjects),
-		// 		},
-		// 	},
-		// })
 	}
 
 	get envFrom(): kplus.EnvFrom[] {
@@ -86,201 +77,30 @@ export class BackendConfig extends Construct {
 	}
 }
 
-export class BackendWSGI extends Component<BackendApiProps> {
-	static componentName = 'wsgi'
-
-	constructor(scope: Construct, id: string, props: BackendApiProps) {
-		super(scope, id, props)
-
-		const staticVolume = kplus.Volume.fromEmptyDir(
-			scope,
-			'static-files',
-			'staticfiles',
+export abstract class ApiComponent<
+	PropsT extends ApiProps = ApiProps,
+> extends Component<PropsT> {
+	static supportsProbes<T extends Construct>(
+		construct: T,
+	): construct is T & IHttpProbable {
+		return (
+			Object.hasOwnProperty.call(construct, 'httpProbePath') &&
+			typeof (construct as T & { httpProbePath: unknown }).httpProbePath ===
+				'string'
 		)
-		// const secretsVolume = kplus.Volume.fromCsi(
-		// 	scope,
-		// 	'secrets',
-		// 	'secrets-store.csi.k8s.io',
-		// 	{
-		// 		readOnly: true,
-		// 		attributes: {
-		// 			secretProviderClass: 'aws-secrets',
-		// 		},
-		// 	},
-		// )
-
-		this.addContainer({
-			name: 'backend',
-			portNumber: 5000,
-			...(props.probes ?? {}),
-			envFrom: props.config.envFrom,
-			securityContext: {
-				readOnlyRootFilesystem: false,
-				user: 1000,
-				group: 1000,
-			},
-			volumeMounts: [
-				{ volume: staticVolume, path: '/app/staticfiles' },
-				// { volume: secretsVolume, path: '/run/secrets' },
-			],
-			command: ['/serve.sh', 'wsgi'],
-		})
-
-		this.addContainer({
-			name: 'migrate',
-			command: ['python', 'manage.py', 'migrate', '--noinput', '--verbosity=1'],
-			init: true,
-			envFrom: props.config.envFrom,
-			securityContext: {
-				readOnlyRootFilesystem: false,
-			},
-		})
-
-		this.addContainer({
-			name: 'collectstatic',
-			command: [
-				'python',
-				'manage.py',
-				'collectstatic',
-				'--link',
-				'--no-post-process',
-				'--noinput',
-				'--verbosity=2',
-			],
-			init: true,
-			envFrom: props.config.envFrom,
-			securityContext: {
-				readOnlyRootFilesystem: false,
-				user: 1000,
-				group: 1000,
-			},
-			volumeMounts: [{ volume: staticVolume, path: '/app/staticfiles' }],
-		})
-	}
-}
-
-export class BackendASGI extends Component<BackendApiProps> {
-	static componentName = 'asgi'
-
-	constructor(scope: Construct, id: string, props: BackendApiProps) {
-		super(scope, id, props)
-		this.addContainer({
-			name: 'backend',
-			command: ['/serve.sh', 'asgi'],
-			portNumber: 5000,
-			...(props.probes ?? {}),
-			envFrom: props.config.envFrom,
-			securityContext: {
-				readOnlyRootFilesystem: false,
-				user: 1000,
-				group: 1000,
-			},
-		})
-	}
-}
-
-export class CeleryBeat extends Component<BackendApiProps> {
-	static componentName = 'celerybeat'
-
-	constructor(scope: Construct, id: string, props: BackendApiProps) {
-		super(scope, id, props)
-		this.addContainer({
-			name: 'celerybeat',
-			command: ['/serve.sh', 'celerybeat'],
-			envFrom: props.config.envFrom,
-			securityContext: { readOnlyRootFilesystem: false },
-		})
 	}
 
-	protected createDeploymentProps(): kplus.DeploymentProps {
-		return { replicas: 1 }
-	}
-}
+	readonly config: IApiConfig
 
-export class CeleryWorkers extends Component<BackendApiProps> {
-	static componentName = 'celeryworker'
-
-	addWorkerQueue(queue: CeleryQueueProps): this {
-		this.addContainer({
-			name: queue.name.replaceAll(',', '-'),
-			command: [
-				'/serve.sh',
-				'celeryworker',
-				'-Q',
-				queue.name,
-				'--concurrency=1',
-				...(queue.args ?? []),
-			],
-			envFrom: this.props.config.envFrom,
-			securityContext: { readOnlyRootFilesystem: false },
-		})
-		return this
-	}
-}
-
-export class Celery extends Construct {
-	beat: CeleryBeat
-	workers: CeleryWorkers
-
-	constructor(
-		scope: Construct,
-		id: string,
-		readonly props: CeleryProps,
+	protected constructor(
+		readonly scope: Construct,
+		readonly id: string,
+		readonly props: PropsT,
 	) {
-		super(scope, id)
-		this.beat = new CeleryBeat(this, 'beat', props)
-		this.workers = new CeleryWorkers(this, 'workers', props)
-		if (props.queues)
-			props.queues.forEach((queue) => this.workers.addWorkerQueue(queue))
-	}
-}
-
-export class AdminWebSocket extends Component<BackendApiProps> {
-	static componentName = 'adminwebsocket'
-
-	constructor(scope: Construct, id: string, props: BackendApiProps) {
 		super(scope, id, props)
-		this.addContainer({
-			name: 'adminwebsocket',
-			command: ['/serve.sh', 'adminwebsocket'],
-			envFrom: this.props.config.envFrom,
-		})
-	}
-}
-
-export class Backend extends Construct {
-	wsgi: BackendWSGI
-	asgi: BackendASGI
-	celery: Celery
-	adminWebSocket: AdminWebSocket
-
-	constructor(scope: Construct, id: string, props: BackendProps) {
-		super(scope, id)
-
-		const config = new BackendConfig(this, 'config', {
-			config: props.config,
-			secrets: props.secrets,
-		})
-
-		this.wsgi = new BackendWSGI(this, 'wsgi', {
-			config,
-			probes: this.createHttpProbes('/health'),
-			...props.wsgi,
-		})
-		this.asgi = new BackendASGI(this, 'asgi', {
-			config,
-			probes: this.createHttpProbes('/ws/health'),
-			...props.asgi,
-		})
-		this.celery = new Celery(this, 'celery', {
-			config,
-			...props.celery,
-		})
-		this.adminWebSocket = new AdminWebSocket(this, 'adminwebsocket', {
-			config,
-			...props.wsgi,
-			replicaCount: 1,
-		})
+		const config = props.config ?? ApiConfig.of(scope)
+		if (!config) throw Error('Failed to resolve ApiConfig!')
+		this.config = config
 	}
 
 	protected createHttpProbes(
@@ -305,5 +125,177 @@ export class Backend extends Construct {
 			readiness: readyProbe,
 			startup: startProbe,
 		}
+	}
+}
+
+export class ApiWSGI
+	extends ApiComponent<ApiWSGIProps>
+	implements IHttpProbable
+{
+	static componentName = 'wsgi'
+	readonly httpProbePath = '/health'
+
+	constructor(scope: Construct, id: string, props: ApiWSGIProps) {
+		super(scope, id, props)
+		const securityContext = new kplus.ContainerSecurityContext({
+			readOnlyRootFilesystem: false,
+			user: 1000,
+			group: 1000,
+			ensureNonRoot: true,
+		})
+
+		const backend = this.addContainer({
+			name: 'gunicorn',
+			portNumber: 5000,
+			securityContext,
+			envFrom: this.config.envFrom,
+			command: [
+				'/serve.sh',
+				'wsgi',
+				'--workers',
+				String(props.workers ?? 2),
+				'--threads',
+				String(props.threads ?? 4),
+				'--worker-class=gthread',
+				'--worker-tmp-dir=/worker-tmp',
+			],
+			...(props.probes ?? this.createHttpProbes(this.httpProbePath)),
+		})
+
+		this.addContainer({
+			name: 'migrate',
+			command: ['python', 'manage.py', 'migrate', '--noinput', '--verbosity=1'],
+			init: true,
+			envFrom: this.config.envFrom,
+			securityContext,
+		})
+
+		const staticInit = this.addContainer({
+			name: 'collectstatic',
+			command: [
+				'python',
+				'manage.py',
+				'collectstatic',
+				'--link',
+				'--no-post-process',
+				'--noinput',
+				'--verbosity=2',
+			],
+			init: true,
+			envFrom: this.config.envFrom,
+			securityContext,
+		})
+
+		const staticVolume = kplus.Volume.fromEmptyDir(
+			scope,
+			'static-files',
+			'staticfiles',
+		)
+		const workerTmpVolume = kplus.Volume.fromEmptyDir(
+			scope,
+			'worker-tmp',
+			'worker-tmp',
+			{
+				medium: kplus.EmptyDirMedium.MEMORY,
+			},
+		)
+
+		// mount volumes
+		staticInit.mount('/app/staticfiles', staticVolume)
+		backend.mount('/app/staticfiles', staticVolume)
+		backend.mount('/worker-tmp', workerTmpVolume)
+	}
+}
+
+export class ApiASGI
+	extends ApiComponent<ApiASGIProps>
+	implements IHttpProbable
+{
+	static componentName = 'asgi'
+	httpProbePath = '/ws/health'
+
+	constructor(scope: Construct, id: string, props: ApiASGIProps) {
+		super(scope, id, props)
+		this.addContainer({
+			name: 'hypercorn',
+			command: ['/serve.sh', 'asgi', '--workers', String(props.workers ?? 2)],
+			portNumber: 5000,
+			envFrom: this.config.envFrom,
+			...(props.probes ?? this.createHttpProbes(this.httpProbePath)),
+			securityContext: {
+				readOnlyRootFilesystem: false,
+				user: 1000,
+				group: 1000,
+			},
+		})
+	}
+}
+
+export class CeleryBeat extends ApiComponent {
+	static componentName = 'celerybeat'
+
+	constructor(scope: Construct, id: string, props: ApiProps) {
+		super(scope, id, props)
+		this.addContainer({
+			name: 'celerybeat',
+			command: ['/serve.sh', 'celerybeat'],
+			envFrom: this.config.envFrom,
+			securityContext: {
+				readOnlyRootFilesystem: false,
+				user: 1000,
+				group: 1000,
+			},
+		})
+	}
+
+	protected createDeploymentProps(): kplus.DeploymentProps {
+		return { replicas: 1 }
+	}
+}
+
+export class CeleryWorker extends ApiComponent<CeleryProps> {
+	static componentName = 'celeryworker'
+
+	constructor(scope: Construct, id: string, props: CeleryProps) {
+		super(scope, id, props)
+
+		const name = props.name ?? props.queues.join('-')
+
+		this.addContainer({
+			name,
+			command: [
+				'/serve.sh',
+				'celeryworker',
+				'-Q',
+				props.queues.join(','),
+				'--concurrency',
+				String(props.concurrency ?? 2),
+				...(props.args ?? []),
+			],
+			envFrom: this.config.envFrom,
+			securityContext: {
+				readOnlyRootFilesystem: false,
+				user: 1000,
+				group: 1000,
+			},
+		})
+	}
+}
+
+export class AdminWebSocket extends ApiComponent {
+	static componentName = 'adminwebsocket'
+
+	constructor(scope: Construct, id: string, props: ApiProps) {
+		super(scope, id, props)
+		this.addContainer({
+			name: 'adminwebsocket',
+			command: ['/serve.sh', 'adminwebsocket'],
+			envFrom: this.config.envFrom,
+			securityContext: {
+				readOnlyRootFilesystem: false,
+				user: 1000,
+				group: 1000,
+			},
+		})
 	}
 }
