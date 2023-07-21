c19475c4ead9bfe59a7dd3af1867ca11f83034fd
Braden Mars <bradenmars@bradenmars.me>
2023-07-20 20:20:26 -0500

    feat(config): implement base config, `getConfig` with options, `defineConfig`
    Signed-off-by: Braden Mars <bradenmars@bradenmars.me>

--- packages/config/src/config.ts
+++ packages/config/src/config.ts
@@ -1,7 +1,26 @@
-import { loadConfig, type ResolvedConfig } from 'c12'
-import type { CrisisCleanupConfig, Config } from './types'
+import { exec } from 'child_process'
+import {
+	loadConfig,
+	createDefineConfig,
+	type LoadConfigOptions,
+	type ResolvedConfig,
+	ConfigLayerMeta,
+} from 'c12'
+import createDebug from 'debug'
+import { type Exact } from 'type-fest'
+import { pickSubsetDeep, transformEnvVars } from './transform.ts'
+import type {
+	ApiAppConfig,
+	ApiAppSecrets,
+	ApiConfig,
+	CrisisCleanupConfig,
+	CrisisCleanupConfigInput,
+	CrisisCleanupConfigMeta,
+} from './types'
 
-const baseAppConfig: Config = {
+const debug = createDebug('@crisiscleanup:config')
+
+const baseAppConfig: ApiAppConfig = {
 	celery: {
 		alwaysEager: false,
 	},
@@ -57,17 +76,131 @@ const baseAppConfig: Config = {
 	},
 }
 
-const baseConfig: CrisisCleanupConfig = {
-	config: baseAppConfig,
+// Local secrets
+const baseAppSecrets: ApiAppSecrets = {
+	aws: {
+		accessKeyId: '',
+		secretAccessKey: '',
+		defaultRegion: 'us-east-1',
+		dynamoStage: 'local',
+	},
+	cloudfront: {
+		privateKey: '',
+		publicKey: '',
+	},
+	jwt: {
+		privateKey: '',
+		publicKey: '',
+	},
+	connectFirst: { password: '' },
+	djangoMandrill: {
+		apiKey: '',
+	},
+	postgres: {
+		host: '172.17.0.1',
+		password: 'crisiscleanup_dev',
+		dbname: 'crisiscleanup_dev',
+		port: 5432,
+		user: 'crisiscleanup_dev',
+	},
+	zendesk: {
+		apiKey: '',
+	},
 }
 
-export const getConfig = async (): Promise<
-	ResolvedConfig<CrisisCleanupConfig>
-> => {
-	const cfg = await loadConfig<CrisisCleanupConfig>({
-		defaultConfig: baseConfig,
-		dotenv: true,
-	})
-	console.log(cfg)
-	return cfg
+export const baseApiConfig: ApiConfig = {
+	config: baseAppConfig,
+	secrets: baseAppSecrets,
 }
+
+export const baseConfig: CrisisCleanupConfig = {
+	ccuStage: 'local',
+	cdkEnvironment: {
+		region:
+			process.env.CDK_DEFAULT_REGION ??
+			process.env.AWS_DEFAULT_REGION ??
+			'us-east-1',
+		account: process.env.CDK_DEFAULT_ACCOUNT!,
+	},
+	api: baseApiConfig,
+}
+
+const getGitRoot = (): Promise<string> =>
+	new Promise((resolve, reject) => {
+		exec('git rev-parse --show-toplevel', (error, stdout, stderr) => {
+			if (error) {
+				reject(error)
+			} else if (stderr) {
+				reject(new Error(stderr))
+			} else {
+				resolve(stdout.trim())
+			}
+		})
+	})
+
+export const loadEnvOverrides = (): CrisisCleanupConfig => {
+	const env = Object.assign({}, process.env)
+	const mappedEnv = transformEnvVars(env) as unknown as CrisisCleanupConfig
+	return pickSubsetDeep(mappedEnv, baseConfig)
+}
+
+export interface GetConfigOptions {
+	/**
+	 * Load and override config values from environment variables.
+	 * @default true
+	 */
+	useEnvOverrides: boolean
+	/**
+	 * Throw error if config fails to resolve.
+	 * @default true
+	 */
+	strict: boolean
+}
+
+type LoadedConfig<
+	OptionsT extends GetConfigOptions,
+	ResolvedT extends ResolvedConfig,
+> = OptionsT['strict'] extends true
+	? Omit<ResolvedT, 'config'> & { config: Exclude<ResolvedT['config'], null> }
+	: ResolvedT
+
+type ResolvedCrisisCleanupConfig = ResolvedConfig<
+	CrisisCleanupConfig,
+	CrisisCleanupConfigMeta
+>
+
+export const getConfig = async <
+	T extends Exact<GetConfigOptions, T> = {
+		useEnvOverrides: true
+		strict: true
+	},
+>(
+	options?: T,
+): Promise<LoadedConfig<T, ResolvedCrisisCleanupConfig>> => {
+	const overridesConfig: Partial<
+		LoadConfigOptions<CrisisCleanupConfig, CrisisCleanupConfigMeta>
+	> = options?.useEnvOverrides ?? true ? { overrides: loadEnvOverrides() } : {}
+	debug('using overrides from env: %O', overridesConfig)
+
+	const cfg = await loadConfig<CrisisCleanupConfig, CrisisCleanupConfigMeta>({
+		name: 'crisiscleanup',
+		defaultConfig: baseConfig,
+		envName: 'CCU_STAGE',
+		cwd: await getGitRoot(),
+		...overridesConfig,
+	})
+	debug('resolved config: %O', cfg)
+
+	if (!cfg.config && (options.strict ?? true)) {
+		throw new Error(
+			'Failed to resolve config and getConfig was called with strict=true',
+		)
+	}
+
+	return cfg as LoadedConfig<T, typeof cfg>
+}
+
+export const defineConfig = createDefineConfig<
+	CrisisCleanupConfigInput,
+	CrisisCleanupConfigMeta
+>()
