d08f15dd17c5a494b13c2e01426eb4679612c1de
Braden Mars <bradenmars@bradenmars.me>
2023-07-22 03:08:52 -0500

    feat(config): utilize private config extends by default
    Signed-off-by: Braden Mars <bradenmars@bradenmars.me>

--- crisiscleanup.config.ts
+++ crisiscleanup.config.ts
@@ -3,9 +3,5 @@ import { defineConfig, baseApiConfig } from '@crisiscleanup/config'
 
 export default defineConfig({
 	$meta: { name: 'crisiscleanup' },
-	api: baseApiConfig,
-	cdkEnvironment: {
-		region: process.env.CDK_DEFAULT_REGION ?? 'us-east-1',
-		account: String(process.env.CDK_DEFAULT_ACCOUNT!),
-	},
+	$extends: ['github:CrisisCleanup/configs'],
 })
