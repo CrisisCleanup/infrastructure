bb9562ddfb26e5420665ea6042c086a56e017405
Braden Mars <bradenmars@bradenmars.me>
2023-07-29 03:22:29 -0500

    feat(stacks.api): add stack config defaults
    Signed-off-by: Braden Mars <bradenmars@bradenmars.me>

---
+++ packages/stacks/api/crisiscleanup.config.ts
@@ -0,0 +1,23 @@
+/// <reference path="src/config.d.ts" />
+
+import { defineConfig } from '@crisiscleanup/config'
+
+export default defineConfig({
+	$meta: { name: 'crisiscleanup' },
+	apiStack: {
+		isolateDatabase: false,
+		eks: {
+			defaultSecretsEncryption: true,
+			coreDnsVersion: 'v1.9.3-eksbuild.5',
+			kubeProxyVersion: 'v1.24.15-eksbuild.1',
+			vpcCniVersion: 'v1.13.3-eksbuild.1',
+			k8s: {
+				version: '1.24',
+			},
+		},
+		database: {
+			engineVersion: '15.3',
+			ioOptimized: false,
+		},
+	},
+})
