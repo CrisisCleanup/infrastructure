14cbd34dc45340187d2d4d373186278e15e57430
Braden Mars <bradenmars@bradenmars.me>
2023-07-29 03:22:01 -0500

    feat(stacks.api): define config types
    Signed-off-by: Braden Mars <bradenmars@bradenmars.me>

---
+++ packages/stacks/api/src/types.ts
@@ -0,0 +1,22 @@
+export interface KubeConfig {
+	readonly version: string
+}
+
+export interface EKSConfig {
+	readonly defaultSecretsEncryption: boolean
+	readonly k8s: KubeConfig
+	readonly coreDnsVersion: string
+	readonly kubeProxyVersion: string
+	readonly vpcCniVersion: string
+}
+
+export interface DatabaseConfig {
+	engineVersion: string
+	ioOptimized: boolean
+}
+
+export interface ApiStackConfig {
+	eks: EKSConfig
+	database: DatabaseConfig
+	isolateDatabase: boolean
+}
