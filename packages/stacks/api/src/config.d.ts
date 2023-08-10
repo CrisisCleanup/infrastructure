5e7694d1281f8e8577c21bdfd8a2630a2a7268f5
Braden Mars <bradenmars@bradenmars.me>
2023-08-10 03:24:16 -0500

    feat(stacks.api): update typeshim with input
    Signed-off-by: Braden Mars <bradenmars@bradenmars.me>

--- packages/stacks/api/src/config.d.ts
+++ packages/stacks/api/src/config.d.ts
@@ -1,6 +1,9 @@
-import type {ApiStackConfig} from './types'
+import type {ApiStackConfig, ApiStackConfigInput} from './schema'
 
 declare module '@crisiscleanup/config' {
+  export interface CrisisCleanupConfigInput {
+    apiStack: ApiStackConfig
+  }
   export interface CrisisCleanupConfig {
     apiStack: ApiStackConfig
   }
