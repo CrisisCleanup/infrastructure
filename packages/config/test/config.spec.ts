4bd0b258daaf62cab9e6f18e7d10c00ed3e219a5
Braden Mars <bradenmars@bradenmars.me>
2023-08-10 00:55:25 -0500

    test(config): add unit test for env overrides on environment configs
    Signed-off-by: Braden Mars <bradenmars@bradenmars.me>

--- packages/config/test/config.spec.ts
+++ packages/config/test/config.spec.ts
@@ -1,12 +1,22 @@
-import { expect, it, describe, vi } from 'vitest'
+import os from 'node:os'
+import { expect, it, describe, vi, beforeEach } from 'vitest'
 import {
 	getConfigDefaults,
 	baseConfig,
 	defineConfig,
 	loadEnvOverrides,
+	getConfig,
+	flattenToScreamingSnakeCase,
 } from '../src/index'
 
 describe('getConfig', () => {
+	beforeEach(() => {
+		Reflect.deleteMetadata(
+			Symbol.for('@crisiscleanup:config:defaults'),
+			baseConfig,
+		)
+	})
+
 	it('returns expected defaults', () => {
 		expect(getConfigDefaults()).toStrictEqual(baseConfig)
 	})
@@ -35,6 +45,52 @@ describe('getConfig', () => {
 		})
 		expect(loadEnvOverrides()).toMatchObject({
 			some: { keyTo: 'override' },
+		})
+		vi.unstubAllEnvs()
+	})
+
+	it('applies env overrides to environment configs', async () => {
+		const envForOverride = flattenToScreamingSnakeCase(getConfigDefaults())
+		Object.keys(envForOverride)
+			.filter((key) => key in process.env)
+			.forEach((key) => vi.stubEnv(key, ''))
+		vi.stubEnv('CCU_STAGE', 'development')
+		vi.stubEnv('CCU_CONFIGS_DECRYPT', 'false')
+		defineConfig({
+			api: { config: { ccu: { apiUrl: 'https://some.url.com' } } },
+			$meta: { name: 'crisiscleanup' },
+			$development: {
+				ccuStage: 'development',
+				api: { config: { ccu: { apiUrl: 'https://some.dev.url.com' } } },
+			},
+		})
+		vi.stubEnv('API__CONFIG__CCU__API_URL', 'https://some.override.url.com')
+		const cfg = await getConfig(
+			{
+				strict: true,
+				useEnvOverrides: true,
+			},
+			{
+				cwd: os.tmpdir(),
+				defaultConfig: {
+					$env: {
+						// @ts-ignore
+						development: {
+							ccuStage: 'development',
+							// @ts-ignore
+							api: { config: { ccu: { apiUrl: 'https://some.dev.url.com' } } },
+						},
+					},
+				},
+			},
+		)
+		expect(cfg.config).toMatchObject({
+			api: { config: { ccu: { apiUrl: 'https://some.override.url.com' } } },
+		})
+		expect(cfg.config.$env).toMatchObject({
+			development: {
+				api: { config: { ccu: { apiUrl: 'https://some.override.url.com' } } },
+			},
 		})
 		vi.unstubAllEnvs()
 	})
