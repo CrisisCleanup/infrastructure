ab16cab25d88a605b001d6afbf8e64b3a6b5a4d3
Braden Mars <bradenmars@bradenmars.me>
2023-08-14 02:06:42 -0500

    feat(charts.ccu): use new config schemas for base charts config
    Signed-off-by: Braden Mars <bradenmars@bradenmars.me>

--- packages/charts/crisiscleanup/crisiscleanup.config.ts
+++ packages/charts/crisiscleanup/crisiscleanup.config.ts
@@ -1,65 +1,6 @@
 import { defineConfig } from '@crisiscleanup/config'
+import { chartConfigSchema } from './src/schema'
 
-const celeryScaling = {
-	scaling: {
-		minReplicas: 1,
-		maxReplicas: 2,
-	},
-}
-
-// Chart config defaults for local development.
 export default defineConfig({
-	$meta: { name: 'crisiscleanup' },
-	chart: {
-		namespace: 'local',
-		domainName: 'local.crisiscleanup.io',
-		apiImage: {
-			repository: 'crisiscleanup-api',
-			tag: 'latest',
-			pullPolicy: 'IfNotPresent',
-		},
-		webImage: {
-			repository: 'crisiscleanup-web',
-			tag: 'latest',
-			pullPolicy: 'IfNotPresent',
-		},
-		frontend: {
-			web: {
-				replicaCount: 2,
-			},
-		},
-		wsgi: {
-			scaling: {
-				minReplicas: 1,
-				maxReplicas: 3,
-			},
-		},
-		asgi: {
-			scaling: {
-				minReplicas: 1,
-				maxReplicas: 2,
-			},
-		},
-		celeryBeat: {
-			replicaCount: 1,
-		},
-		celery: {
-			celery: { queues: ['celery'], ...celeryScaling },
-			signal: {
-				queues: ['signal', 'phone', 'metrics'],
-				...celeryScaling,
-			},
-		},
-		ingressAnnotations: {
-			'alb.ingress.kubernetes.io/listen-ports': '[{"HTTP": 80}, {"HTTPS":443}]',
-			'alb.ingress.kubernetes.io/ssl-redirect': '443',
-			'alb.ingress.kubernetes.io/scheme': 'internet-facing',
-			'alb.ingress.kubernetes.io/target-type': 'ip',
-			'alb.ingress.kubernetes.io/target-group-attributes':
-				'load_balancing.algorithm.type=least_outstanding_requests',
-			'alb.ingress.kubernetes.io/healthcheck-path': '/health',
-			'alb.ingress.kubernetes.io/load-balancer-attributes':
-				'idle_timeout.timeout_seconds=120',
-		},
-	},
+	chart: chartConfigSchema.parse({}),
 })
