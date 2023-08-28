14d14757b90ea8a91b08a79918f29d8bf953a210
Braden Mars <bradenmars@bradenmars.me>
2023-08-28 00:53:08 -0500

    feat(projenrc): implement `CdkTsAppCompileBuilder`
    Signed-off-by: Braden Mars <bradenmars@bradenmars.me>

---
+++ projenrc/builders.ts
@@ -0,0 +1,42 @@
+import {
+	type BuildOptions,
+	type BuildOutput,
+	BuildStep,
+	type TypedPropertyDescriptorMap,
+} from '@arroyodev-llc/utils.projen-builder'
+import { NodePackageUtils } from '@aws-prototyping-sdk/nx-monorepo'
+import { type awscdk, type ProjectOptions } from 'projen'
+
+export class CdkTsAppCompileBuilder extends BuildStep<{}, {}> {
+	constructor(private options?: { synthPostCompileCondition?: string }) {
+		super()
+	}
+
+	applyOptions(
+		options: ProjectOptions & BuildOptions<this>,
+	): ProjectOptions & BuildOptions<this> {
+		return options
+	}
+
+	applyProject(
+		project: awscdk.AwsCdkTypeScriptApp,
+	): TypedPropertyDescriptorMap<BuildOutput<this>> {
+		project.addGitIgnore('cdk.context.json')
+		project.cdkConfig.json.addOverride(
+			'app',
+			NodePackageUtils.command.exec(
+				project.package.packageManager,
+				'tsx',
+				'src/main.ts',
+			),
+		)
+		const postCompile = project.tasks.tryFind('post-compile')!
+		postCompile.reset()
+		postCompile.spawn(project.tasks.tryFind('synth:silent')!, {
+			condition:
+				this.options?.synthPostCompileCondition ??
+				`bash -c '[[ -z "$SKIP_SYNTH" ]]'`,
+		})
+		return {} as TypedPropertyDescriptorMap<BuildOutput<this>>
+	}
+}
