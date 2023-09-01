6b112c79cad8e327691e92425f4249cf955556b3
Braden Mars <bradenmars@bradenmars.me>
2023-08-31 22:41:50 -0500

    test(charts.ccu): impl chart prop cases and targets for testing variations of chart configurations
    Signed-off-by: Braden Mars <bradenmars@bradenmars.me>

--- packages/charts/crisiscleanup/test/main.spec.ts
+++ packages/charts/crisiscleanup/test/main.spec.ts
@@ -1,39 +1,67 @@
 import { baseConfig } from '@crisiscleanup/config'
-import { Testing } from 'cdk8s'
+import { type App, Chart, Testing } from 'cdk8s'
 import { ImagePullPolicy } from 'cdk8s-plus-27'
-import { test, expect, describe, it } from 'vitest'
+import defu from 'defu'
+import { describe, expect, it } from 'vitest'
 import defaultChartConfig from '../crisiscleanup.config'
-import { CrisisCleanupChart } from '../src'
+import { CrisisCleanupChart, type CrisisCleanupChartProps } from '../src'
 
-describe('CrisisCleanupChart', () => {
+const buildChart = (app: App, values: Partial<CrisisCleanupChartProps>) => {
+	return CrisisCleanupChart.withDefaults(
+		app,
+		defu(
+			values as CrisisCleanupChartProps,
+			{
+				...defaultChartConfig.chart,
+				apiAppConfig: baseConfig.api.config,
+				apiAppSecrets: baseConfig.api.secrets,
+				apiImage: {
+					repository: 'test-api',
+					tag: 'test',
+					pullPolicy: ImagePullPolicy.ALWAYS,
+				},
+				webImage: {
+					repository: 'test-web',
+					tag: 'test',
+					pullPolicy: ImagePullPolicy.ALWAYS,
+				},
+				namespace: 'test',
+				domainName: 'test.crisiscleanup.io',
+				frontend: { web: { replicaCount: 1 } },
+			} as unknown as CrisisCleanupChartProps,
+		),
+	)
+}
+
+const propCases: [
+	name: string,
+	props?: Partial<CrisisCleanupChartProps>,
+	chartIds?: string[],
+][] = [
+	['defaults'],
+	[
+		'with spread',
+		{
+			asgi: { spread: true, replicaCount: 4 },
+			wsgi: { spread: true, replicaCount: 4 },
+		},
+	],
+]
+
+describe.each(propCases)('CrisisCleanupChart: %o', (caseName, props, keys) => {
 	const app = Testing.app()
-	const chart = CrisisCleanupChart.withDefaults(app, {
-		...defaultChartConfig.chart,
-		apiAppConfig: baseConfig.api.config,
-		apiAppSecrets: baseConfig.api.secrets,
-		apiImage: {
-			repository: 'test-api',
-			tag: 'test',
-			pullPolicy: ImagePullPolicy.ALWAYS,
-		},
-		webImage: {
-			repository: 'test-web',
-			tag: 'test',
-			pullPolicy: ImagePullPolicy.ALWAYS,
-		},
-		namespace: 'test',
-		domainName: 'test.crisiscleanup.io',
-		frontend: { web: { replicaCount: 1 } },
-	})
+	const chart = buildChart(app, props ?? {})
+	const children = chart.node.children
+		.filter((c) => Chart.isChart(c))
+		.filter((subchart) => (keys ? keys.includes(subchart.node.id) : true))
+	const targets = [chart, ...children]
 
-	it.each([
-		['chart', chart],
-		['web', chart.webChart],
-		['api', chart.apiChart],
-		['celery', chart.celeryChart],
-		['namespace', chart.namespaceChart],
-		['config', chart.configChart],
-	])('%s: matches snapshot', (name, c) => {
-		expect(Testing.synth(c)).toMatchSnapshot(name)
-	})
+	it.each(targets.map((targ) => [targ.node.id, targ]))(
+		`%s matches snapshot`,
+		(name, target) => {
+			expect(Testing.synth(target as Chart)).toMatchSnapshot(
+				caseName + '-' + name,
+			)
+		},
+	)
 })
