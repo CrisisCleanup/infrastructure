957fe01b41589ee7f12232f76f836a8f9330f688
Braden Mars <bradenmars@bradenmars.me>
2023-09-22 02:00:09 -0500

    feat(stacks.web): add environment vars to pipeline
    Signed-off-by: Braden Mars <bradenmars@bradenmars.me>

--- packages/stacks/web/src/main.ts
+++ packages/stacks/web/src/main.ts
@@ -5,6 +5,7 @@ import {
 	interpolateValue,
 } from '@crisiscleanup/construct.awscdk.github-pipeline'
 import { App } from 'aws-cdk-lib'
+import * as ghpipelines from 'cdk-pipelines-github'
 import { GitHubStage, type GitHubStageProps } from 'cdk-pipelines-github'
 import type { Construct } from 'constructs'
 import { CrisisCleanupWeb, type CrisisCleanupWebProps } from './web'
@@ -58,21 +59,83 @@ const pipeline = GithubCodePipeline.create({
 		repository: 'CrisisCleanup/crisiscleanup-4-web',
 	})
 	.addSynthEnv({
+		VITE_APP_API_BASE_URL: interpolateValue(
+			ActionsContext.VARS,
+			'VITE_APP_API_BASE_URL',
+		),
+		VITE_APP_BASE_URL: interpolateValue(
+			ActionsContext.VARS,
+			'VITE_APP_BASE_URL',
+		),
+		VITE_APP_WS_URL: interpolateValue(ActionsContext.VARS, 'VITE_APP_WS_URL'),
+		VITE_APP_AWS_CCP_URL: interpolateValue(
+			ActionsContext.VARS,
+			'VITE_APP_AWS_CCP_URL',
+		),
+		VITE_APP_CCP_INSTANCE: interpolateValue(
+			ActionsContext.VARS,
+			'VITE_APP_CCP_INSTANCE',
+		),
+		VITE_APP_STAGE: interpolateValue(ActionsContext.VARS, 'VITE_APP_STAGE'),
+		VITE_APP_PORTAL_KEY: interpolateValue(
+			ActionsContext.VARS,
+			'VITE_APP_PORTAL_KEY',
+		),
+		VITE_APP_PHONE_DEFAULT_USERNAME: interpolateValue(
+			ActionsContext.VARS,
+			'VITE_APP_PHONE_DEFAULT_USERNAME',
+		),
+		VITE_APP_PHONE_DEFAULT_PASSWORD: interpolateValue(
+			ActionsContext.VARS,
+			'VITE_APP_PHONE_DEFAULT_PASSWORD',
+		),
+		VITE_APP_ENGLISH_PHONE_GATEWAY: interpolateValue(
+			ActionsContext.VARS,
+			'VITE_APP_ENGLISH_PHONE_GATEWAY',
+		),
+		VITE_APP_SPANISH_PHONE_GATEWAY: interpolateValue(
+			ActionsContext.VARS,
+			'VITE_APP_SPANISH_PHONE_GATEWAY',
+		),
+		VITE_APP_DEFAULT_CALLER_ID: interpolateValue(
+			ActionsContext.VARS,
+			'VITE_APP_DEFAULT_CALLER_ID',
+		),
+		VITE_APP_CRISISCLEANUP_WEB_CLIENT_ID: interpolateValue(
+			ActionsContext.VARS,
+			'VITE_APP_CRISISCLEANUP_WEB_CLIENT_ID',
+		),
+		NODE_ENV: interpolateValue(ActionsContext.VARS, 'NODE_ENV'),
 		CCU_CONFIGS_DECRYPT: 'true',
 		CCU_WEB_SITE_SOURCE:
-			interpolateValue(ActionsContext.GITHUB, 'workspace') + '/dist',
+			interpolateValue(ActionsContext.GITHUB, 'workspace') +
+			'/.crisiscleanup-4-web/dist',
 	})
 	.synthTarget({
 		packageName: 'stacks.web',
 		workingDirectory: 'packages/stacks/web',
+	})
+	.synthPreStep({
+		name: 'Build Web',
+		run: 'pushd .crisiscleanup-4-web && pnpm install && pnpm build:app',
+	})
+	.synthCheckout({
+		ref: 'master',
+		repository: 'CrisisCleanup/crisiscleanup-4-web',
+		path: '.crisiscleanup-4-web',
 	})
 	.defaultTools()
 	.clone({
 		workflowTriggers: {},
 	})
 	.build(app)
-	.onWorkflowCall()
-	.onWorkflowDispatch()
+	.onWorkflowCall({
+		environment: {
+			type: 'string',
+			description: 'Environment to deploy.',
+			default: 'development',
+		},
+	})
 	.concurrency({ group: 'deploy-web', cancelInProgress: false })
 
 pipeline.addStageWithGitHubOptions(
