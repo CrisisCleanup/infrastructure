9415ecce5c422a21286cd83832390ef2e539e4e9
Braden Mars <bradenmars@bradenmars.me>
2023-10-10 04:01:06 -0500

    feat(k8s.construct.api): implement `DatabaseSync` construct for api db sync
    Signed-off-by: Braden Mars <bradenmars@bradenmars.me>

---
+++ packages/k8s/construct/api/src/sync.ts
@@ -0,0 +1,71 @@
+import {
+	ContainerImage,
+	type ContainerImageProps,
+} from '@crisiscleanup/k8s.construct.component'
+import { Cron, type CronOptions, Duration } from 'cdk8s'
+import * as kplus from 'cdk8s-plus-27'
+import { RestartPolicy } from 'cdk8s-plus-27'
+import { Construct } from 'constructs'
+
+export interface DatabaseSyncTargetConfig {
+	readonly bastionHost: string
+	readonly bastionKey: string
+	readonly databaseDsn: string
+	readonly dev: boolean
+}
+
+export interface DatabaseSyncProps
+	extends Partial<Omit<kplus.CronJobProps, 'schedule'>> {
+	readonly image: ContainerImageProps
+	readonly schedule: CronOptions
+	readonly target: DatabaseSyncTargetConfig
+	readonly sourceDsn?: string | null
+}
+
+export class DatabaseSync extends Construct {
+	readonly syncCronJob: kplus.CronJob
+
+	constructor(scope: Construct, id: string, props: DatabaseSyncProps) {
+		super(scope, id)
+
+		const configVolume = kplus.Volume.fromEmptyDir(
+			this,
+			'config-volume',
+			'config',
+			{
+				medium: kplus.EmptyDirMedium.MEMORY,
+			},
+		)
+
+		const syncArgs = [
+			'manage.py',
+			'pgsync_db',
+			'--config-path=/config/pg.yaml',
+			`--target-bastion=${props.target.bastionHost}`,
+			`--target-bastion-key=${props.target.bastionKey}`,
+			`--target-dsn=${props.target.databaseDsn}`,
+		]
+		if (props.target.dev) {
+			syncArgs.push('--dev')
+		}
+		if (props.sourceDsn) {
+			syncArgs.push(`--source-dsn=${props.sourceDsn}`)
+		}
+		const syncContainer = new kplus.Container({
+			name: 'sync',
+			...ContainerImage.fromProps(props.image).containerProps,
+			command: ['python'],
+			args: syncArgs,
+		})
+		syncContainer.mount('/config', configVolume)
+
+		this.syncCronJob = new kplus.CronJob(this, id + '-job', {
+			schedule: Cron.schedule(props.schedule),
+			ttlAfterFinished: Duration.hours(2),
+			restartPolicy: RestartPolicy.NEVER,
+			backoffLimit: 2,
+			containers: [syncContainer],
+		})
+		this.syncCronJob.addVolume(configVolume)
+	}
+}
