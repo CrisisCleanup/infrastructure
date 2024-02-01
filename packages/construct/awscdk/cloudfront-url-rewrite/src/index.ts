6815570b065c1b0023e5798f06f11dbd87ca7970
Braden Mars <bradenmars@bradenmars.me>
2024-02-01 02:40:18 -0600

    feat(construct.awscdk.cloudfront-url-rewrite): `CloudFrontUrlRewrite` construct for applying cf function to distribution
    Signed-off-by: Braden Mars <bradenmars@bradenmars.me>

--- packages/construct/awscdk/cloudfront-url-rewrite/src/index.ts
+++ packages/construct/awscdk/cloudfront-url-rewrite/src/index.ts
@@ -1,5 +1,5 @@
-export class Hello {
-	public sayHello() {
-		return 'hello, world!'
-	}
-}
+export * from './cloudfront-url-rewrite'
+export {
+	type HandlerLambdaCloudFrontFunctionProps as CloudFrontRewriteFunctionProps,
+	HandlerLambdaCloudFrontFunction as CloudFrontRewriteFunction,
+} from './handler.cfn-function.ts'
