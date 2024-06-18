c552167beec34270eab25fe63eb17b10eab59f9f
Braden Mars <bradenmars@bradenmars.me>
2024-06-18 16:07:37 -0500

    test(stacks.maintenance-stack): add unit test for passing domainName
    Signed-off-by: Braden Mars <bradenmars@bradenmars.me>

--- packages/stacks/maintenance-site/test/main.spec.ts
+++ packages/stacks/maintenance-site/test/main.spec.ts
@@ -17,4 +17,20 @@ describe('MaintenanceSite', () => {
 		const template = Template.fromStack(stack)
 		expect(template.toJSON()).toMatchSnapshot()
 	})
+
+	test('renders expected template with different domain name', async () => {
+		const sourcePath = path.join(os.tmpdir(), 'maintenance-test')
+		const tempDir = await fs.mkdtemp(sourcePath)
+		const app = new App()
+		const stack = new MaintenanceSite(app, 'test-maintenance-site', {
+			source: tempDir,
+			domainName: 'crisiscleanup.io.au',
+		})
+		const template = Template.fromStack(stack)
+		const jsonTemplate = template.toJSON()
+		const strTemplate = JSON.stringify(jsonTemplate)
+		expect(strTemplate.includes('crisiscleanup.org')).not.toBeTruthy()
+		expect(strTemplate.includes('maintenance.crisiscleanup.io.au')).toBeTruthy()
+		expect(jsonTemplate).toMatchSnapshot()
+	})
 })
