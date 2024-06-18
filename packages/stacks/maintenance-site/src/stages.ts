e688bf42d0bffdba0f396940936398a844c767e9
Braden Mars <bradenmars@bradenmars.me>
2024-06-18 16:20:25 -0500

    feat(stacks.maintenance-site): add `domainName`, `name, `description` stage props to `MaintenanceStage`
    Signed-off-by: Braden Mars <bradenmars@bradenmars.me>

--- packages/stacks/maintenance-site/src/stages.ts
+++ packages/stacks/maintenance-site/src/stages.ts
@@ -2,21 +2,46 @@ import { GitHubStage, type GitHubStageProps } from 'cdk-pipelines-github'
 import type { Construct } from 'constructs'
 import { MaintenanceSite } from './maintenance-site.ts'
 
+export interface MaintenanceStageProps extends GitHubStageProps {
+	/**
+	 * Domain name to use.
+	 * @default crisiscleanup.org
+	 */
+	readonly domainName?: string
+	/**
+	 * Informative name of stage/stack.
+	 * @default maintenance-site
+	 */
+	readonly name?: string
+	/**
+	 * Informative description of stage/stack.
+	 * @default Maintenance Site
+	 */
+	readonly description?: string
+}
+
 export class MaintenanceStage extends GitHubStage {
-	constructor(scope: Construct, id: string, props: GitHubStageProps) {
-		super(scope, id, props)
+	constructor(scope: Construct, id: string, props: MaintenanceStageProps) {
+		const {
+			domainName,
+			name = 'maintenance-site',
+			description = 'Maintenance Site',
+			...restProps
+		} = props
+		super(scope, id, restProps)
 		const source = process.env.MAINTENANCE_SITE_SOURCE
 		if (!source) throw new Error('MAINTENANCE_SITE_SOURCE is required')
 		new MaintenanceSite(
 			this,
-			'maintenance-site',
+			name,
 			{
 				source: source,
+				domainName: domainName,
 			},
 			{
 				env: props.env,
-				description: 'Maintenance Site',
-				stackName: 'maintenance-site',
+				description: description,
+				stackName: name,
 			},
 		)
 	}
