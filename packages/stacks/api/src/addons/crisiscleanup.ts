c86cd0c0d6926b76b4d605868d3e48fb60dca8f5
Braden Mars <bradenmars@bradenmars.me>
2024-10-04 17:21:35 -0500

    fix(stacks.api): make `CrisisCleanupAddOn` depend on `VerticalPodAutoscalerAddon`
    Signed-off-by: Braden Mars <bradenmars@bradenmars.me>

--- packages/stacks/api/src/addons/crisiscleanup.ts
+++ packages/stacks/api/src/addons/crisiscleanup.ts
@@ -16,6 +16,7 @@ import { App, type Chart } from 'cdk8s'
 import * as kplus from 'cdk8s-plus-27'
 import type { Construct } from 'constructs'
 import defu from 'defu'
+import { VerticalPodAutoscalerStackAddOn } from './vertical-pod-autoscaler'
 import { type NamedSecretsProvider } from '../secrets'
 import { getRequiredResource } from '../util'
 
@@ -38,6 +39,7 @@ export class CrisisCleanupAddOn implements blueprints.ClusterAddOn {
 		// TODO: resolve esbuild transform error that is occurring with tsx/esbuild.
 		const newDeploy = blueprints.utils.dependable(
 			blueprints.addons.SecretsStoreAddOn.name,
+			VerticalPodAutoscalerStackAddOn.name,
 		)(
 			// eslint-disable-next-line @typescript-eslint/no-unsafe-argument
 			Object.getPrototypeOf(this),
