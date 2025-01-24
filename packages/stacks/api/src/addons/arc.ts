2e90bf5b98997da7dc71343edfce2a2f48246934
Braden Mars <bradenmars@bradenmars.me>
2025-01-23 19:43:01 -0600

    feat(stacks.api/addons/arc): update helm chart to v0.10.1
    Signed-off-by: Braden Mars <bradenmars@bradenmars.me>

--- packages/stacks/api/src/addons/arc.ts
+++ packages/stacks/api/src/addons/arc.ts
@@ -91,7 +91,7 @@ const scaleSetControllerDefaultProps: blueprints.HelmAddOnProps &
 		'oci://ghcr.io/actions/actions-runner-controller-charts/gha-runner-scale-set-controller',
 	release: 'arc',
 	namespace: 'arc-systems',
-	version: '0.9.2',
+	version: '0.10.1',
 	values: {},
 }
 
